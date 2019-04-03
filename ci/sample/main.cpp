98096e7415cdcfb869b107ceab2735c1110cafba
Corentin Le Molgat <corentinl@google.com>
2019-04-03 15:46:32 +0200

    Add CMake CI

---
+++ ci/sample/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <coin/CoinUtilsConfig.h>
+#include <coin/OsiConfig.h>
+#include <coin/CbcConfig.h>
+
+int main(int argc, char** argv) {
+  std::cout << "coinutils version: " << COINUTILS_VERSION << std::endl;
+  std::cout << "osi version: " << OSI_VERSION << std::endl;
+  std::cout << "cbc version: " << CBC_VERSION << std::endl;
+	return 0;
+}
+
